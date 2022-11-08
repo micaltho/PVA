e7bbcf50098b70f64461d932077ba13a4e790fe1
Michael Althoff <86599274+micaltho@users.noreply.github.com>
2022-11-08 18:51:36 +0100

    Add files via upload

---
+++ PVA_P3 (Dijkstra)/build/CMakeFiles/main_Dijkstra.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_Dijkstra.dir/Dijkstra.cpp.o"
+  "CMakeFiles/main_Dijkstra.dir/Dijkstra.cpp.o.d"
+  "main_Dijkstra"
+  "main_Dijkstra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_Dijkstra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
