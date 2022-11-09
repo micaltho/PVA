cd5ad17d56d674e6077be81109aaa591108068d1
Michael Althoff <86599274+micaltho@users.noreply.github.com>
2022-11-09 18:18:58 +0100

    Add files via upload

---
+++ build/CMakeFiles/main_gewinn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_gewinn.dir/main_gewinn.o"
+  "CMakeFiles/main_gewinn.dir/main_gewinn.o.d"
+  "main_gewinn"
+  "main_gewinn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_gewinn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/main_sequentiell.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_sequentiell.dir/main_sequentiell.o"
+  "CMakeFiles/main_sequentiell.dir/main_sequentiell.o.d"
+  "main_sequentiell"
+  "main_sequentiell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_sequentiell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
